ec8377b39723099d373d63bc94c01147f6ce4d45
Shreyak <007shreyak@gmail.com>
2020-09-29 12:37:08 +0530

    second commit

---
+++ build/precache-manifest.01dbe6f664fe4bf2a340efe15e8b2662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f3b2623c334eb60a8373779e254793",
+    "url": "/personalWeb/index.html"
+  },
+  {
+    "revision": "91a124a69b47ad9c5709",
+    "url": "/personalWeb/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "65d26f275f924a7da4d1",
+    "url": "/personalWeb/static/js/2.afa6dbb2.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/personalWeb/static/js/2.afa6dbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a124a69b47ad9c5709",
+    "url": "/personalWeb/static/js/main.6f662491.chunk.js"
+  },
+  {
+    "revision": "c1567ff3e251720cfc6a",
+    "url": "/personalWeb/static/js/runtime-main.830448d9.js"
+  }
+]);
